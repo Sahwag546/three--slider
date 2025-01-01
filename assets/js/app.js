5eb5ecd2b5f3dbf9f8e6bb869a67f2c829a7a531
Sahwag546 <sahwagkantiwal494@gmail.com>
2025-01-01 15:41:09 +0530

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,95 @@
+
+// slider-1 
+$('.third-slider').slick({
+    dots: true,
+    arrow: false,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplaySpeed: 3000,
+    centermode: true,
+    prevArrow: $('#prev'),
+    nextArrow: $('#next'),
+     
+    responsive: [
+
+        {
+            breakpoint: 992,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        },
+        {
+            breakpoint: 786,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+    ]
+    
+});
+// slider-2
+
+$('.center').slick({
+  centerMode: true,
+  centerPadding: '60px',
+    slidesToShow: 3,
+    variableWidth: true,
+    slidesToShow: 3,
+    autoplay: true,
+    autoplaySpeed:0,
+    speed: 3000,
+    pauseOnHover: true,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    }
+  ]
+});
+//  third slider details 
+$('.one-time').slick({
+    dots: false,
+    arrows: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 6,
+    adaptiveHeight: true,
+       prevArrow: ".prev",
+    nextArrow: ".next",
+  responsive: [
+    {
+      breakpoint: 1300,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 5
+      }
+      },
+      {
+      breakpoint: 1200,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 4
+      }
+    },
+  ]
+});
